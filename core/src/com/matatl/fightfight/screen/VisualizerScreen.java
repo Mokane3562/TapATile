d5e1c1a51f8fecb1ddb212ca10f3e6702c269122
Mokane3562 <mikesingleton.24@gmail.com>
2015-01-03 17:41:17 -0330

    Added a test visualizer screen. Useless unless I can get Gtomee visualizer to work.

---
+++ core/src/com/matatl/fightfight/screen/VisualizerScreen.java
@@ -0,0 +1,60 @@
+package com.matatl.fightfight.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.matatl.fightfight.TextureManager;
+import com.matatl.fightfight.camera.OrthoCamera;
+
+/**
+ * Created by Mokane on 1/3/2015.
+ *
+ * A would be visualizer test screen. never used.
+ */
+public class VisualizerScreen extends Screen {
+    private OrthoCamera camera;
+
+    @Override
+    public void create() {
+        camera = new OrthoCamera();
+        camera.resize();
+        System.out.println("MenuScreen created");
+    }
+
+    @Override
+    public void update() {
+        camera.update();
+        System.out.println("MenuScreen updated");
+    }
+
+    @Override
+    public void render(SpriteBatch spriteBatch) {
+        spriteBatch.setProjectionMatrix(camera.combined);
+        spriteBatch.begin();
+
+        spriteBatch.end();
+        System.out.println("MenuScreen rendered");
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        camera.resize();
+        System.out.println("MenuScreen resized");
+    }
+
+    @Override
+    public void dispose() {
+        System.out.println("MenuScreen disposed");
+    }
+
+    @Override
+    public void pause() {
+        System.out.println("MenuScreen paused");
+    }
+
+    @Override
+    public void resume() {
+        System.out.println("MenuScreen resumed");
+    }
+}
