5d6223102321530caff3e854b933a0ec8483b410
pmsc73 <philipmstc@gmail.com>
2015-01-04 00:02:47 -0330

    songs?

--- core/src/com/matatl/fightfight/tile/KillTile.java
+++ core/src/com/matatl/fightfight/tile/KillTile.java
@@ -3,8 +3,8 @@ package com.matatl.fightfight.tile;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
+import com.matatl.fightfight.AudioManager;
 import com.matatl.fightfight.ScoreManager;
-import com.matatl.fightfight.SoundManager;
 import com.matatl.fightfight.TextureManager;
 import com.matatl.fightfight.camera.OrthoCamera;
 import com.matatl.fightfight.screen.GameScreen;
@@ -23,7 +23,7 @@ public class KillTile extends Tile{
     }
     public void handleTouch(Vector2 touch){
         if (this.containsPoint(touch)){
-            SoundManager.KILL_TILE_FX.play();
+            AudioManager.KILL_TILE_FX.play();
             ScreenManager.setScreen(new MenuScreen());
         }
     }
